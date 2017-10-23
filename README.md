e50f1751ef460189ce5ef5ae7659dd8173540915
Andre Rendeiro <afrendeiro@gmail.com>
2017-10-23 17:09:41 +0200

    add more options; fix date range filtering

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ git2labnotebook
 ===============
 
 Forced to write a lab notebook while only doing computer work?
+
+Parse all your commits from various projects and make them into daily summaries of your activity, ready to print into a lab notebook. Yay \o/
+
--- git2labnotebook/git2labnotebook.py
+++ git2labnotebook/git2labnotebook.py
@@ -1,10 +1,22 @@
+#!/usr/bin/env python
 
-from argparse import ArgumentParser
+
 import datetime
-import git
-import pandas as pd
 import os
 import sys
+from argparse import ArgumentParser
+
+import git
+import pandas as pd
+
+
+def valid_date(s):
+    import argparse
+    try:
+        return datetime.datetime.strptime(s, "%Y%m%d")
+    except ValueError:
+        msg = "Not a valid date: '{0}'.".format(s)
+        raise argparse.ArgumentTypeError(msg)
 
 
 def parse_arguments():
@@ -19,25 +31,37 @@ def parse_arguments():
         dest="repos_dir",
         default=os.path.abspath(os.path.curdir),
         help="Directory with git repositories to inspect. Default=current directory.")
-    parser.add_argument(
-        '-u', '--git-user', dest="git_user",
-        default=None,
-        help="Git username to filter commits for (all others will be ignored). Default None.")
     today = datetime.datetime.now().strftime('%Y%m%d')
     parser.add_argument(
         '-o', '--output-dir', dest="output_dir",
         default=os.path.join(os.path.expanduser("~"), "git2labnotebook", today),
-        help="Git username to filter commits for (all others will be ignored).")
+        help="Directory to write output to. Will be created if not existing. Default is the $HOME/git2labnotebook/{}.".format(today))
     parser.add_argument(
         '-f', '--from', dest="date_from",
-        default=None,
-        help="Earlier date to filter entries to. Default=None.")
+        type=valid_date, default=None,
+        help="Earlier date to filter entries to. Format=YYYYMMDD. Default=None.")
     parser.add_argument(
         '-t', '--to', dest="date_to",
-        default=today,
-        help="Later date to filter entries to. Default=None.")
+        type=valid_date, default=today,
+        help="Later date to filter entries to. Format=YYYYMMDD. Default=None.")
+    parser.add_argument(
+        '-u', '--git-user', dest="git_user",
+        default=None,
+        help="Git username to filter commits for (all others will be ignored). Default None.")
+    parser.add_argument(
+        '-fr', '--filter-repos', dest="repo_filter",
+        default=None,
+        help="Comma-separated list of repositories to ignore. Default None.")
+    parser.add_argument(
+        '--no-summary', dest="daily_summary", action="store_false",
+        help="Whether daily entries will not contain a summary of lines changed. Default=False.")
+    parser.add_argument(
+        '--fill', dest="fill", action="store_true",
+        help="Whether workdays without entries should be filled with random messages from a pool. Default=False.")
 
     args = parser.parse_known_args()
+    if args[0].repo_filter is not None:
+        args[0].repo_filter = args[0].repo_filter.split(",")
 
     return args
 
@@ -45,7 +69,6 @@ def parse_arguments():
 def get_commits(repos_dir):
     """
     """
-
     output = pd.DataFrame()
     for repo in os.listdir(repos_dir):
         repo_dir = os.path.join(repos_dir, repo, ".git")
@@ -103,7 +126,9 @@ def parse_git_log_stats(log):
         entries, columns=["hash", "author", "date", "message", "file_name", "change_type", "change"])
 
 
-def gitlog2text(git_log, min_day=None, max_day=None):
+def gitlog2text(git_log, min_day=None, max_day=None, daily_summary=True, fill_messages=False):
+    """
+    """
     t_day_header = """# {date}\n"""
     t_repo = """### "{repo}" project.\n"""
     t_commit = """     - {message}"""
@@ -119,20 +144,32 @@ def gitlog2text(git_log, min_day=None, max_day=None):
 
     if min_day is None:
         min_day = git_log.index.min()
+    else:
+        min_day = pd.to_datetime(min_day).to_pydatetime()
     if max_day is None:
         max_day = git_log.index.max()
+    else:
+        max_day = pd.to_datetime(max_day).to_pydatetime()
 
     text = list()
     for day in pd.date_range(min_day, max_day, freq="D"):
-        day_commits = git_log.ix[git_log.index[git_log.index == day.date()]]
-
-        if day_commits.shape[0] < 1:
-            if day.isoweekday() < 6:
+        try:
+            day_commits = git_log.ix[day.date()]
+        except KeyError:
+            if fill_messages:
                 text.append(t_day_header.format(date=day.date()))
                 text.append(t_commit.format(message="Reading papers.\n"))
-                continue
             else:
                 continue
+
+        if fill_messages:
+            if day_commits.shape[0] < 1:
+                if day.isoweekday() < 6:
+                    text.append(t_day_header.format(date=day.date()))
+                    text.append(t_commit.format(message="Reading papers.\n"))
+                    continue
+                else:
+                    continue
 
         if day.isoweekday() < 6:
             text.append(t_day_header.format(date=day.date()))
@@ -141,7 +178,8 @@ def gitlog2text(git_log, min_day=None, max_day=None):
             text.append(t_repo.format(repo=repo))
             for commit in day_commits[day_commits["repository"] == repo]["hash"].drop_duplicates():
                 text.append(t_commit.format(message=git_log[git_log["hash"] == commit]["message"].drop_duplicates().squeeze()))
-        text.append(t_day_footer.format(daily_changes[day.date().isoformat()]["del"], daily_changes[day.date().isoformat()]["ins"]))
+        if daily_summary:
+            text.append(t_day_footer.format(daily_changes.loc[day.date().isoformat(), "del"], daily_changes.loc[day.date().isoformat(), "ins"]))
 
     return text
 
@@ -153,14 +191,24 @@ def main():
     # Get commits for all repositories
     git_log = get_commits(args.repos_dir)
 
+    # Filter by user if required
     if args.git_user:
         git_log = git_log[git_log["author"].str.contains(args.git_user)]
 
+    # Filter projects if required
+    if args.git_user:
+        git_log = git_log[~git_log["repository"].str.contains("|".join(args.repo_filter))]
+
+    # Save log
     if not os.path.exists(args.output_dir):
         os.makedirs(args.output_dir)
     git_log.to_csv(os.path.join(args.output_dir, "git_log.csv"), index=False, encoding="utf-8")
 
-    text = gitlog2text(git_log)
+    # Make text
+    text = gitlog2text(
+        git_log,
+        min_day=args.date_from, max_day=args.date_to,
+        daily_summary=args.daily_summary, fill_messages=args.fill)
 
     with open(os.path.join(args.output_dir, "notebook.md"), "w") as handle:
         handle.writelines("\n".join(text))
